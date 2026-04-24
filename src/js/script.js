22e239aa1022b87ad1a58e2bddc0a2652344c2e2
spencerajo <spencerajo@gmail.com>
2026-04-24 15:02:37 -0500

    added comments

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,10 @@
-const b = document.getElementById("mybutton");
-b.onclick = show;
+const b = document.getElementById("mybutton"); //Create button 'b'.
+b.onclick = show; //Upon clicking the button, call the 'show()' function.
 
 function show(){
-    const showntext = document.createElement("h2");
-    showntext.innerText = "Hi Spencer!";
+    const showntext = document.createElement("h2"); //Create a new, empty h2 element.
+    showntext.innerText = "Hi Spencer!"; //Fill the h2 element with text.
 
-    const textcontainer = document.getElementById("container");
-    textcontainer.appendChild(showntext);
+    const textcontainer = document.getElementById("container"); //Get the div container to hold the new text.
+    textcontainer.appendChild(showntext); //Put the new h2 element in the div container.
 }
