6b024ec367f0ce5ca2b1a27ea5ada88244903859
spencerajo <spencerajo@gmail.com>
2026-04-27 16:25:16 -0500

    Created Textbox to display username.

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,11 @@
 const b = document.getElementById("mybutton"); //Create button 'b'.
-b.onclick = show; //Upon clicking the button, call the 'show()' function.
+b.onclick = handleClick; //Upon clicking the button, call the function.
 
-function show(){
+function handleClick(){
+    const username = document.getElementById("userInput").value; //Get the value of the text that is input into the textbox.
     const showntext = document.createElement("h2"); //Create a new, empty h2 element.
-    showntext.innerText = "Hi Spencer!"; //Fill the h2 element with text.
+
+    showntext.innerText = "Hello, " + username + "!"; //Replace everything in the h2 element with this text.
 
     const textcontainer = document.getElementById("container"); //Get the div container to hold the new text.
     textcontainer.appendChild(showntext); //Put the new h2 element in the div container.
