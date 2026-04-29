78f7d5fef50ca62ff46a88c5c11dc938725dc28c
spencerajo <spencerajo@gmail.com>
2026-04-29 11:09:17 -0500

    fixed errors & added clear screen function

--- src/js/UserInputButton/script.js
+++ src/js/UserInputButton/script.js
@@ -2,11 +2,13 @@ const b = document.getElementById("mybutton"); //Create button 'b'.
 b.onclick = handleClick; //Upon clicking the button, call the function.
 
 function handleClick(){
+    const textcontainer = document.getElementById("container"); //Get the div container to hold the new text. 
+    textcontainer.innerText = ""; //Clear text that is present from previous runs.
+
     const username = document.getElementById("userInput").value; //Get the value of the text that is input into the textbox.
     const showntext = document.createElement("h2"); //Create a new, empty h2 element.
 
     showntext.innerText = "Hello, " + username + "!"; //Replace everything in the h2 element with this text.
 
-    const textcontainer = document.getElementById("container"); //Get the div container to hold the new text.
     textcontainer.appendChild(showntext); //Put the new h2 element in the div container.
 }
